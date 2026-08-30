dfe7c48e3f5ea006bbf453a7923f76a3cb2f76bc
Michael Johnston <lastobelus@mac.com>
2026-08-30 16:17:11 -0700

    fix(mobile): restore Action resume notice import

--- apps/mobile/src/features/threads/ThreadDetailScreen.tsx
+++ apps/mobile/src/features/threads/ThreadDetailScreen.tsx
@@ -70,6 +70,7 @@ import type {
 } from "../../lib/threadActivity";
 import { PendingApprovalCard } from "./PendingApprovalCard";
 import { PendingUserInputCard } from "./PendingUserInputCard";
+import { ActionResumeNotice } from "./ActionResumeNotice";
 import {
   FLOATING_WORKING_CONTROL_COVERAGE,
   FloatingWorkingControl,
