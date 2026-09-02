ced0c71a4c92fd50ef7da6ee834f6a54ef73fbff
Michael Johnston <lastobelus@mac.com>
2026-09-02 01:53:39 -0700

    fix(lastcode): format nightly 1256 revision

--- apps/desktop/src/electron/ElectronProtocol.test.ts
+++ apps/desktop/src/electron/ElectronProtocol.test.ts
@@ -225,13 +225,7 @@ describe("ElectronProtocol", () => {
       "http:",
       "https:",
     ]);
-    assert.deepEqual(directives["media-src"], [
-      "'self'",
-      "lastcode:",
-      "blob:",
-      "http:",
-      "https:",
-    ]);
+    assert.deepEqual(directives["media-src"], ["'self'", "lastcode:", "blob:", "http:", "https:"]);
     assert.deepEqual(directives["font-src"], ["'self'", "lastcode:", "data:"]);
   });
 });
